26049b3c1e4b874d0a7ba842c781a99808b647b0
Antonio <unmendezantonio@gmail.com>
2024-06-29 22:56:06 -0600

    helpme

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import { any } from "joi";
 
 
 export default [
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  any
 ];
