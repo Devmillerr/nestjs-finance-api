884e3160f11fce97c9ac1715269c6aef4ac3a72e
yordin21 <1260493@senati.pe>
2024-06-30 20:00:58 -0500

    USERDETAILS_CREATE_SCHEMA

--- src/utils/schema/user.ts
+++ src/utils/schema/user.ts
@@ -1,8 +1,19 @@
 import Joi from 'joi'
+import { join } from 'path'
 
 export const USER_CREATE_SCHEMA = Joi.object({
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
 })
 
-export const USERDETAILS_CREATE_SCHEMA = ""
+export const USERDETAILS_CREATE_SCHEMA = Joi.object({
+    firstname: Joi.string().min(1).max(255).required(),
+    lastname : Joi.string().min(1).max(255).required(),
+    nickname: Joi.string().min(1).max(255),
+    zipcode: Joi.string().min(1).max(255),
+    address: Joi.string().min(1).max(255),
+    phone: Joi.string().pattern(RegExp(/^[0-9+\-().\s]{7,15}$/)),
+})
+
+export const PRODUCT_CREATE_SCHEMA = ""
+
