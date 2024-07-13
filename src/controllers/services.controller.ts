c3b55daf08a8bed351e319e2b40f1311ea665c38
neenbyssp <neenbyss@gmail.com>
2024-07-12 23:03:13 -0600

    controller and services - services

---
+++ src/controllers/services.controller.ts
@@ -0,0 +1,16 @@
+import { Response, Request, NextFunction } from 'express'
+import boom from '@hapi/boom'
+
+// TODO: TERMINARLO
+
+export class ServicesController {
+  async getAll(req: Request, res: Response) {}
+
+  async getOne(req: Request, res: Response, next: NextFunction) {}
+
+  async create(req: Request, res: Response) {}
+
+  async update(req: Request, res: Response) {}
+
+  async remove(req: Request, res: Response) {}
+}
---
+++ src/services/database/services.services.ts
@@ -0,0 +1,47 @@
+import { db } from '../db'
+import { Prisma } from '@prisma/client'
+
+export class ServicesServices {
+  create(data: Prisma.servicesCreateArgs) {
+    return db.services.create(data)
+  }
+  getOne(where: Prisma.servicesWhereInput, include: Prisma.servicesInclude) {
+    return db.services.findFirst({ where, include })
+  }
+  getAll() {
+    return db.services.findMany()
+  }
+  update(
+    where: Prisma.servicesWhereUniqueInput,
+    data: Prisma.servicesUpdateInput
+  ) {
+    return db.services.update({ data, where })
+  }
+  remove(where: Prisma.servicesWhereUniqueInput) {
+    return db.services.delete({ where })
+  }
+
+  createContract(data: Prisma.service_contractsCreateArgs) {
+    return db.service_contracts.create(data)
+  }
+
+  getOneContract(
+    where: Prisma.service_contractsWhereInput,
+    include: Prisma.servicesInclude
+  ) {
+    return db.service_contracts.findFirst({ where, include })
+  }
+
+  getAllContract() {
+    return db.service_contracts.findMany()
+  }
+
+  updateContract(
+    where: Prisma.service_contractsWhereUniqueInput,
+    data: Prisma.service_contractsUpdateInput
+  ) {
+    return db.service_contracts.update({ where, data })
+  }
+
+  // TODO: terminarlo
+}
